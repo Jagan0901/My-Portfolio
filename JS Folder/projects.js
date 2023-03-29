f9c9149453891466a58089bc73d98b0d1dcde341
Jagan0901 <Jaganath0901@gmail.com>
2023-03-29 14:37:19 +0530

    added project

--- JS Folder/projects.js
+++ JS Folder/projects.js
@@ -18,6 +18,18 @@ projectSection.innerHTML =
     
   </div>
 
+  <div class="box">
+    <a href="https://gold-calculator-india.netlify.app" target="_blank"><img src="https://img.freepik.com/free-vector/desktop-smartphone-app-development_23-2148683810.jpg?size=338&ext=jpg&ga=GA1.2.1661719016.1671630009&semt=ais" alt=""></a>
+    <h1>name : <span>Gold Calculator </span></h1>
+    <h1>Technologies used : <span>React, MongoDB, Express, NodeJS</span></h1>
+    <h1>Concepts used: <span> API methods, CRUD operations, JWT(for authentication) </span></h1>
+    <h1>Github links:   
+      
+    <a href="https://github.com/Jagan0901/Gold-Calculator-Front-End.git" target="_blank"><p> front-End<i class="fa-brands fa-github"></i></p></a>
+    <a href="https://github.com/Jagan0901/Gold-Calculator-Back-End.git" target="_blank"><p>: back-End<i class="fa-brands fa-github"></i></p></h1></a>
+    
+  </div>
+
 
 
 </div>`
