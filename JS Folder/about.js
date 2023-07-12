03b30d3ff30887a809b02fd41683dfa4ffac8cc1
Jagan0901 <Jaganath0901@gmail.com>
2023-07-12 22:49:36 +0530

    made few changes

--- JS Folder/about.js
+++ JS Folder/about.js
@@ -7,6 +7,6 @@ aboutSection.innerHTML = `  <h1 class="heading"><span>about </span>Me</h1>
 <div class="row">
         
    <p>Creative and responsible Mathematics undergraduate student passionate about programming and looking for exciting opportunities to learn, grow and gain experience in the field of full stack development. If I get an opportunity, I'll give my one hundred percent to express my skills and experiences in technologies to do the work. I believe it will help me upskill myself on the technical aspects and contribute to great improvement in my career growth in the future as well.</p>
-   <a href="https://drive.google.com/file/d/1fkV_AzDRaej-09ivvZIl5Eib-5sYwvpH/view" target="_blank"><button class="btn">Resume <i class="fas fa-download"></i></button></a>
+   <a href="https://drive.google.com/file/d/1eI-We0nRnCGbuZswffq11TDZPrS8Jhf5/view" target="_blank"><button class="btn">Resume <i class="fas fa-download"></i></button></a>
 </div>`;
 document.body.appendChild(aboutSection);
