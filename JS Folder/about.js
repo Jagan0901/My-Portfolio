baf9b957f232419454b536219bea944064b2dc19
Jagan0901 <110884222+Jagan0901@users.noreply.github.com>
2024-01-23 21:03:56 +0530

    updated

--- JS Folder/about.js
+++ JS Folder/about.js
@@ -6,7 +6,7 @@ aboutSection.innerHTML = `  <h1 class="heading"><span>about </span>Me</h1>
 
 <div class="row">
         
-   <p>Creative and responsible Mathematics undergraduate student passionate about programming and looking for exciting opportunities to learn, grow and gain experience in the field of full stack development. If I get an opportunity, I'll give my one hundred percent to express my skills and experiences in technologies to do the work. I believe it will help me upskill myself on the technical aspects and contribute to great improvement in my career growth in the future as well.</p>
+   <p>Creative and responsible Mathematics undergraduate student passionate about programming and looking for exciting opportunities to learn, grow and gain experience in the field of full stack development. I am a fresher. However, I have more than one year of experience in developing MERN full stack web development and has a 3 months experience as a web developer. If I get an opportunity, I'll give my one hundred percent to express my skills and experiences in technologies to do the work. I believe it will help me upskill myself on the technical aspects and contribute to great improvement in my career growth in the future as well.</p>
    <a href="https://drive.google.com/file/d/1ijqnymPFYKkvxzTpiQwfPgYawM6DoCoY/view" target="_blank"><button class="btn">Resume <i class="fas fa-download"></i></button></a>
 </div>`;
 document.body.appendChild(aboutSection);
